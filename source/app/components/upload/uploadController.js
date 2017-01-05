420d144eb44d9687b188a226e579c22911224eec
tin <tin@rasia.info>
2017-01-05 14:39:00 +0700

    add modal upload

--- source/app/components/upload/uploadController.js
+++ source/app/components/upload/uploadController.js
@@ -2,6 +2,17 @@ app.controller('uploadCtrl', ['$scope', '$rootScope', '$http', 'ConfigService', 
     '$window', 'md5', 'loginService','$timeout', function($scope, $rootScope, $http, ConfigService, $state, $window,
      md5, loginService,$timeout) {
     var host = ConfigService.host;
-
+    
+	$("#file-1").fileinput({
+		uploadUrl: '#', // you must set a valid URL here else you will get an error
+		allowedFileExtensions : ['jpg', 'png','gif'],
+		overwriteInitial: false,
+		maxFileSize: 1000,
+		maxFilesNum: 10,
+		//allowedFileTypes: ['image', 'video', 'flash'],
+		slugCallback: function(filename) {
+		    return filename.replace('(', '_').replace(']', '_');
+		}
+	});
 
 }]);
