31d76fd932c587e50f02771223406763d0727855
tin <tin@rasia.info>
2017-01-06 13:30:37 +0700

    show img list

--- source/app/components/gallery/galleryController.js
+++ source/app/components/gallery/galleryController.js
@@ -5,17 +5,21 @@ app.controller('galleryCtrl', ['$scope', '$rootScope', '$http', 'ConfigService',
     $scope.loggedUser = angular.fromJson($window.localStorage.loggedUser);
     var uid = $scope.isLogin = $scope.loggedUser.id;
     $scope.listImages = [];
+    $scope.detectImg ;
     $http.get(host + '/api/images/'+uid).then(function(res){
         console.log(res.data.data)
         $scope.listImages = res.data.data;
     })
 
-    $scope.detectImage = function(){
+    $scope.detectImage = function(img){
+    	$scope.detectImg = img ;
     	 var url = host + "/api/detect/";
     	 var filePath = "source/uploads/02YHkd0mrEqg3Vnp6HOn7EcW.jpg";
+    	 var filePath = img.path;
     	 var imgObj = {filePath:filePath};
     	$http.post(url,imgObj).then(function(res){
-    		console.log(res);
+    		$scope.detectImg.tags = res.data.data;
+    		console.log($scope.detectImg.tags);
     	})
     }
 }]);
