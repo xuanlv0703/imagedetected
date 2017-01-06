8a202ca8d373d7cd8d80ffbe99cf6c673945c816
xuan <xuan@rasia.info>
2017-01-06 13:17:16 +0700

    get list image

--- source/app/components/gallery/galleryController.js
+++ source/app/components/gallery/galleryController.js
@@ -2,6 +2,13 @@ app.controller('galleryCtrl', ['$scope', '$rootScope', '$http', 'ConfigService',
     '$window', 'md5', 'loginService','$timeout', function($scope, $rootScope, $http, ConfigService, $state, $window,
      md5, loginService,$timeout) {
     var host = ConfigService.host;
+    $scope.loggedUser = angular.fromJson($window.localStorage.loggedUser);
+    var uid = $scope.isLogin = $scope.loggedUser.id;
+    $scope.listImages = [];
+    $http.get(host + '/api/images/'+uid).then(function(res){
+        console.log(res.data.data)
+        $scope.listImages = res.data.data;
+    })
 
     $scope.detectImage = function(){
     	 var url = host + "/api/detect/";
--- source/app/components/upload/uploadController.js
+++ source/app/components/upload/uploadController.js
@@ -2,14 +2,15 @@ app.controller('uploadCtrl', ['$scope', '$rootScope', '$http', 'ConfigService', 
     '$window', 'md5', 'loginService','$timeout','Upload', function($scope, $rootScope, $http, ConfigService, $state, $window,
      md5, loginService,$timeout,Upload) {
     var host = ConfigService.host;
-
+    $scope.loggedUser = angular.fromJson($window.localStorage.loggedUser);
+    var uid = $scope.isLogin = $scope.loggedUser.id;
 	$("#file-1").fileinput({
 		uploadUrl: '/api/upload', // you must set a valid URL here else you will get an error
 		allowedFileExtensions : ['jpg', 'png','gif'],
 		overwriteInitial: false,
 		maxFileSize: 1000,
 		maxFilesNum: 10,
-		uploadExtraData: {id: 1, value: '100 Details'},
+		uploadExtraData: {id: uid, value: '100 Details'},
 		//allowedFileTypes: ['image', 'video', 'flash'],
 		slugCallback: function(filename) {
 		    return filename.replace('(', '_').replace(']', '_');
