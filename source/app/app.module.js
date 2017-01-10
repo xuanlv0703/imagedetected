35a27766b26f8c5a7cb6d96dff150083c3e6027f
xuan <xuan@rasia.info>
2017-01-10 11:25:02 +0700

    hostname

--- source/app/app.module.js
+++ source/app/app.module.js
@@ -2,8 +2,8 @@ var app = angular.module('myApp', ['ui.router', 'datatables', 'angular-md5','ngF
     'ui.bootstrap.datetimepicker','ui-notification','ngSanitize', 'ui.select','checklist-model']);
 app.factory('ConfigService', [function() {
     return {
-        host: 'http://localhost:9013',
-        hostImage:'http://localhost:9012'    
+        host: 'http://'+location.hostname+':9013',
+        hostImage:'http://'+location.hostname+':9012',  
     };
 }]);
 
