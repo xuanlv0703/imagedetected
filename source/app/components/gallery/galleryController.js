ab01a242cb8df5418914d65bed9f4a37dfa81535
tin <tin@rasia.info>
2017-01-11 16:11:08 +0700

    filter color

--- source/app/components/gallery/galleryController.js
+++ source/app/components/gallery/galleryController.js
@@ -11,24 +11,30 @@ app.controller('galleryCtrl', ['$scope', '$rootScope', '$http', 'ConfigService',
     	$scope.filter.album = $scope.listAlbum.map(function(a){return a.id});
     })
 
-    $scope.listImages = [];
-    $scope.listTags = [];
+    $scope.listImages   = [];
+    $scope.listTags     = [];
+    $scope.listColors   = [];
     $http.get(host + '/api/images/'+uid).then(function(res){
     	$scope.listImages = res.data.data.map(function(img){
     		img.isShow = true ;
             img.tags = img.tags == null ?[]:(img.tags.length ? img.tags.split(";") : []);
             img.city = img.city == null ?[]:(img.city.length ? img.city.split(";") : []);
-    		$scope.addTags(img.tags);
+            $scope.addTags(img.tags);
+            $scope.filter.tags = $scope.listTags.slice();
+            $scope.addColor(img.colors);
     		return img;
     	});
-    	$scope.listTags = $scope.listTags.sort();
+        $scope.listTags = $scope.listTags.sort();
+    	$scope.listColors = $scope.listColors.sort().reverse();
     })
 
     // filter
     $scope.filter = {};
     $scope.filter.album = [];
+    $scope.filter.color = '';
     $scope.allAlbum = true;
     $scope.allTags = true;
+    $scope.allColor = true;
 
     $scope.checkAllTags = function(){
         if($scope.allTags){
@@ -60,6 +66,21 @@ app.controller('galleryCtrl', ['$scope', '$rootScope', '$http', 'ConfigService',
         $scope.filter.album = [];
     };
 
+    $scope.addTags = function(tags){
+        tags.map(function(tag){
+            if ($scope.listTags.indexOf(tag) === -1  ) {
+                $scope.listTags.push(tag);
+            } 
+        });
+    }
+
+    $scope.addColor = function(color){
+        if ($scope.listColors.indexOf(color) === -1  ) {
+            $scope.listColors.push(color);
+        } 
+        // $scope.filter.color = $scope.listColors.slice();
+    }
+
     // edit
     $scope.detectImg ;
     $scope.isDetecting      = false;
@@ -74,9 +95,9 @@ app.controller('galleryCtrl', ['$scope', '$rootScope', '$http', 'ConfigService',
         $scope.tagAvailable     = $scope.detectImg.tags;
         $scope.cityAvailable    = $scope.detectImg.city;
         // first detect for img
-        if(!$scope.detectImg.isDetected){
-            $scope.detectImage();
-        }
+        // if(!$scope.detectImg.isDetected){
+        //     $scope.detectImage();
+        // }
 
     }
     $scope.detectImage = function(){
@@ -103,15 +124,6 @@ app.controller('galleryCtrl', ['$scope', '$rootScope', '$http', 'ConfigService',
         })
     }
 
-    $scope.addTags = function(tags){
-    	tags.map(function(tag){
-	    	if ($scope.listTags.indexOf(tag) === -1  ) {
-		        $scope.listTags.push(tag);
-		    } 
-    	});
-    	$scope.filter.tags = $scope.listTags.slice();
-    }
-
     $scope.updateImg = function(){
         var imgid= $scope.detectImg.id;
         var url = host+ '/api/images/'+imgid;
@@ -120,12 +132,12 @@ app.controller('galleryCtrl', ['$scope', '$rootScope', '$http', 'ConfigService',
                 if(img.id === $scope.detectImg.id){
                     img.title   = $scope.detectImg.title ;
                     img.tags    = $scope.detectImg.tags ;
+                    $scope.addTags(img.tags);
                     img.city    = $scope.detectImg.city ;
                     img.colors  = $scope.detectImg.colors ;
                 }
             })
         })
-
     }
 
 
